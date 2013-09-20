12de99b98be21b786db6a195d8a419790416d146
Marco Laspe <marco@rockiger.com>
2013-09-20 13:10:51 +0200

    Changed absolute path of todo file

--- js/services.js
+++ js/services.js
@@ -1,10 +1,13 @@
 var org = require('./lib/markdown-org-mode-parser');
+var util = require("./js/util");
 var fs = require("fs")
+
 
 angular.module('myApp').factory('Nodelist',  function() {
 
   var nodes;
-  var data = fs.readFileSync("/home/macco/.livewookiee/liveflow.md", "utf-8");
+  var currentFile = util.getTaskFiles();
+  var data = fs.readFileSync(currentFile, "utf-8");
   nodes = org.parseBigString(data);
   return nodes;
 });
