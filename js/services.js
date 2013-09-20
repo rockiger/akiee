f580b91f42636c132eed95d9fc711eeea28c28d8
Marco Laspe <marco@rockiger.com>
2013-09-20 10:23:29 +0200

    Restructured App

--- js/services.js
+++ js/services.js
@@ -1,5 +1,4 @@
-var org = require('/home/macco/Listings/org-mode-parser/lib/org-mode-parser');
-//var org = require('./lib/markdown-org-mode-parser');
+var org = require('./lib/markdown-org-mode-parser');
 var fs = require("fs")
 
 angular.module('myApp').factory('Nodelist',  function() {
@@ -7,6 +6,5 @@ angular.module('myApp').factory('Nodelist',  function() {
   var nodes;
   var data = fs.readFileSync("/home/macco/.livewookiee/liveflow.md", "utf-8");
   nodes = org.parseBigString(data);
-  console.log(nodes);
   return nodes;
 });
