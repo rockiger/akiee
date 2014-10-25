e5643a7c8a13ab9dcf1c3c8b3df46af14cb3416f
rockiger <marco@rockiger.com>
2014-10-26 01:02:02 +0200

    Rewrote the rank change on the model and file-level. Moved away from manipulating the content of the file in the ace editor, to manipulating a ListOfNodes, tha represents all tasks. Then writing these ListOfNodes to the markdown file. This is a much better and error prone way. Funny that it took me so long to see it.

--- js/updateRank.js
+++ js/updateRank.js
@@ -2,6 +2,10 @@
 "use strict";
 var assert = require("assert");
 var enterTask = require("./enterTask");
+var util = require("./util");
+
+
+var deepEqual = assert.deepEqual;
 
 /*
  * ==========
@@ -19,7 +23,7 @@ var deepEqual = assert.deepEqual;
  * set the rank of el one higher
  */
 function upRank(el, ES, ED, showTask) {
-    moveRank(el, ES, ED, showTask, "up");    
+    moveRank(el, ES, ED, showTask, "up");
 }
 
 /* DomElement EditorSession Editor Function -> Void
@@ -33,9 +37,14 @@ function downRank(el, ES, ED, showTask) {
  * move the rank of el 
  */
 function moveRank(el, ES, ED, showTask, upOrDown) {
+    var content = ED.getSession().getValue();
+    var lon = util.getNodes(content);
+    
     var currentRow = el.parentNode;
     var currentHeadline = currentRow.children[1].innerHTML;
     var currentStatus = currentRow.children[0].innerHTML;
+    var currentNode = nodeWithHeadline(lon, currentHeadline, currentStatus);
+    var currentRank = parseInt(currentNode.rank);
     
     if (upOrDown === "up") {
         try {
@@ -57,37 +66,73 @@ function moveRank(el, ES, ED, showTask, upOrDown) {
         }
     }
     
-    // get RANK
-    var currentRank = getRank(currentHeadline, ES, ED);
-    var borderRank = getRank(borderHeadline, ES, ED);
+    var borderNode = nodeWithHeadline(lon, borderHeadline, currentStatus);
+    var borderRank = parseInt(borderNode.rank);
     
-    if (typeof borderRank !== "number" | borderRank === undefined) {
+    if (borderRank === "" | borderRank === undefined) {
         console.log(borderHeadline + " has no Rank.");
         return;
     }
     
-    console.log(typeof currentRank + " : " + currentRank);
-    if (typeof currentRank !== "number" | currentRank === undefined) {
+    if (currentRank === "" | currentRank === undefined) {
         // current Headline is not rated yet, create ranking
         // currentRank = enterTask.newRank();
         console.log(currentHeadline + " has no Rank.");
         return;
     }
     
-    // get ranges of ranks between current and border
     if (upOrDown === "up") {
-        var lor = getListOfUpperRanges(borderRank, currentRank, ED, ES);
-        decListOfRangesOfRank(lor, ED, ES);
+        var lon = lon.map(function(key, val, array) {
+            if (parseInt(key.rank) >= borderRank && parseInt(key.rank) < currentRank) {
+                var rank = parseInt(key.rank) + 1;
+                key.rank = rank.toString();
+            }
+            return key;
+        });
     } else {
-        var lor = getListOfLowerRanges(borderRank, currentRank, ED, ES);
-        incListOfRangesOfRank(lor, ED, ES);
+       var lon = lon.map(function(key, val, array) {
+                if (parseInt(key.rank) <= borderRank && parseInt(key.rank) > currentRank) {
+                    var rank = parseInt(key.rank) - 1;
+                    key.rank = rank.toString();
+                }
+                return key;
+            });     
     }
-    // give current tast the new upper rank
-    updateTaskRank(currentHeadline, currentRank, borderRank, ED, ES);
+    currentNode.rank = borderRank;
+    
+    //console.log(currentRank);
+    //console.log(borderRank);
+    //console.log(lon);
+    
+    var editorContent = "";
+    lon.forEach(function(e) {
+       if (e.level === 1) {
+           editorContent += "# ";
+       } else if (e.level === 2) {
+           editorContent += "## ";
+       }
+       
+       if (e.todo) {
+           editorContent += e.todo + " ";
+       }
+       
+       editorContent += e.headline  + "\n";
+       
+       if (e.body) {
+           editorContent += e.body + "\n";
+       }
+       
+       if (e.rank) {
+           editorContent += RANK + e.rank + "\n";
+       }
+    });
+    
+    //console.log(editorContent);
+    
+    ES.setValue(editorContent.trim());
     
     // update view 
     showTask(currentStatus);
-    
 }
 
 /* String Number Number Editor Session -> Void
@@ -105,12 +150,13 @@ function updateTaskRank(headline, oldRank, newRank, ED) {
  */
 function getRank(headline, ES, ED) {
     // find range
+    //console.log(headline);
     var headlineRange = ED.find(headline, {wrap:true, range: null}, false);
     
     var beginRank = ED.find(RANK, {wrap:false, range: null}, false);
     //var endRank = ED.find("\s", {wrap:true, range: null}, false);
     var endRank = ED.find("\n#", {wrap:true, range: null}, false); 
-    console.log(headline + ": " + endRank.end);
+    //console.log(headline + ": " + endRank.end);
     var rankRange = beginRank;
     try {
         rankRange.start.column = rankRange.end.column;
@@ -128,7 +174,7 @@ function getRank(headline, ES, ED) {
         // TODO: Add a Rank statement to task, to not puzzle the user
         return undefined;
     }
-    
+    //console.log(rankRange);
     var doc = ES.getDocument();
     var rank = parseInt(doc.getTextRange(rankRange));
     return rank;
@@ -193,6 +239,18 @@ function incListOfRangesOfRank(lor, ED, ES) {
                    ES.replace(el, RANK + newRank); 
                 });
             });
+}
+
+/* ListOfNodes String String ->  Node
+ * consumes a ListOfNodes a headline and a status, returns the found node or undefined
+ */
+deepEqual(nodeWithHeadline([{"headline":"Head 1", "todo":"DOING"},{"headline":"Head 2", "todo":"DOING"}], "Head 1", "DOING"), {"headline":"Head 1", "todo":"DOING"});
+deepEqual(nodeWithHeadline([{"headline":"Head 1", "todo":"DOING"},{"headline":"Head 2", "todo":"DOING"}], "Not Head", "DOING"), undefined);
+
+function nodeWithHeadline(lon, headline, status) {
+    return lon.filter(function (e) {
+            return (e.headline === headline&& e.todo === status);
+        })[0];
 }
 
 exports.upRank = upRank;
