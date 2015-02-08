f5f404c224b39e07eaa881404d0443667f450849
rockiger <marco@rockiger.com>
2015-02-08 14:24:38 +0100

    change buildscript for switch to web2exe

--- scripts/build_binaries.sh
+++ scripts/build_binaries.sh
@@ -1,14 +1,17 @@
-# build with grunt
-grunt
+# Script for building Akiee binaries
+# Usage: run from base dir: ./scripts/build_binaries.sh
+
+# TODO build with Web2Executable
+
 # copy libudev.so.0 and akiee-script to linux64 and linux63
-cp libudev.so.0-64 ./dist/Akiee/linux64/libudev.so.0
-cp libudev.so.0-32 ./dist/Akiee/linux32/libudev.so.0
-cp akiee ./dist/Akiee/linux32/
-cp akiee ./dist/Akiee/linux64/
+cp libudev.so.0-64 ./dist/Akiee/linux-x64/libudev.so.0
+cp libudev.so.0-32 ./dist/Akiee/linux-x32/libudev.so.0
+cp akiee ./dist/Akiee/linux-x32/
+cp akiee ./dist/Akiee/linux-x64/
 
 # create archives for download
 cd ./dist/Akiee/
-tar -zcvf akiee-0.0.1-linux64.tar.gz linux64/
-tar -zcvf akiee-0.0.1-linux32.tar.gz linux32/
-zip -r akiee-0.0.1-osx.zip osx/
-zip -r akiee-0.0.1-win.zip win/
+tar -zcvf akiee-0.0.2-linux64.tar.gz linux-x64/
+tar -zcvf akiee-0.0.2-linux32.tar.gz linux-x32/
+zip -r akiee-0.0.2-osx.zip mac-x64/
+zip -r akiee-0.0.2-win.zip windows-x64/
